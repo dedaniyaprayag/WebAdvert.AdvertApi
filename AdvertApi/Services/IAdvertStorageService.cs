3ffe391b83407175eb748767fa846e6b224fc4b5
dedaniyaprayag <dedaniyaprayag@gmail.com>
2020-08-31 18:40:12 -0400

    Adevert API intial Commit

---
+++ AdvertApi/Services/IAdvertStorageService.cs
@@ -0,0 +1,24 @@
+﻿using AdvertApi.Models;
+using System.Threading.Tasks;
+
+namespace AdvertApi.Services
+{
+    interface IAdvertStorageService
+    {
+        Task<string> Add(AdvertModel model);
+        Task<bool> Confirm(ConfirmAdvertModel model);
+    }
+
+    public class DynamoDBAdvertStorage : IAdvertStorageService
+    {
+        public Task<string> Add(AdvertModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> Confirm(ConfirmAdvertModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
